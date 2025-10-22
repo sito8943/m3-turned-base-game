8d9b4a77c5dbb26b47374d62f1e5ea4bfee46be4
Sito8943 <sito8943@gmail.com>
2025-10-22 16:49:37 +0200

    m3 game

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Gradient from "ink-gradient";
+import BigText from "ink-big-text";
+
+export const App = () => (
+  <Gradient name="summer">
+    <BigText text="M3 Game" align="center" />
+  </Gradient>
+);
